867e329d6271b1c30133cf7ad7f8db417e972661
Getahun <getitpro@gmail.com>
2022-01-16 23:52:52 +0100

    final

--- BishBosh2/bishbosh.js
+++ BishBosh2/bishbosh.js
@@ -1,35 +1,55 @@
 let bishEvent = document.querySelector('#myBtn');
 let boshEvent = document.querySelector('#bashInput');
-
+let bishEvent2 = document.querySelector('#bishInput2');
 
 bishEvent.addEventListener('click', function(e){
-     let countLoop = 0;
+ 
      let countBish = 0;
      let countBosh = 0;
-     let countBishBosh = 0;
+    
      let bish = [];
      let bosh = [];
-     let bishBosh = [];
-    for(let i = 1; i <= boshEvent.value; i++){
-        if(i % 3 === 0 && i % 4 === 0){
-            //console.log("bish-bosh");
-            bishBosh.push(i);
-            countBishBosh++;
-        }else if ((i % 3) === 0){
-            //console.log("bish");
-            bish.push(i);
-            countBish++;
-        } else if ((i % 4) === 0){
-            //console.log("bosh");
-            bosh.push(i);
+   
+    let i = 1;
+    let j = 1; 
+    //select the divible numbers by 3 between 1 and the given max number
+    if(bishEvent2.value == 3){
+        
+         while(i <= boshEvent.value){
+            if((i % 3) === 0){
+                
+                bish.push(i);
+                countBish++;
+                } 
+                i++;              
+        }
+
+        // display the bish numbers
+    document.querySelector('#firsthead').innerHTML =  `<p> Bish: ${bish} Total bish numbers: ${countBish} </p>`;
+  
+} 
+//select the divible numbers by 3 between 1 and the given max number
+else if(bishEvent2.value == 4){
+        
+        while(j <= boshEvent.value){
+            if((j % 4) === 0){
+            
+            bosh.push(j);
             countBosh++;
+           } 
+           j++;      
         } 
-        countLoop++;
-    }
-    
-   document.querySelector('#firsthead').innerHTML +=  `<p> Bish: ${bish} Total bish numbers: ${countBish} </p>`;
-   document.querySelector('#firsthead').innerHTML +=  `<p> Bosh: ${bosh} Total bish numbers: ${countBosh} </p>`;
-   document.querySelector('#firsthead').innerHTML +=  `<p> Bish-Bosh: ${bishBosh} Total bish numbers: ${countBishBosh} </p>`;
+    // display the bosh numbers
+      document.querySelector('#firsthead').innerHTML =  `<p> Bosh: ${bosh} Total bosh numbers: ${countBosh} </p>`;     
+   }
+   else
+   {
+          alert("wrong input");
+   }   
+ // remove the input values after execution 
+  boshEvent.value = '';
+  bishEvent2.value = '';
+  
 });
 
 
--- ToDoList/toDoList.js
+++ ToDoList/toDoList.js
@@ -1,23 +1,40 @@
 let listInput = document.querySelector('#inList');
 let listBtn = document.querySelector('#btn');
 let listP = document.querySelector('#pList');
-
+let bodyEvent = document.querySelector('#bodyId');
+const items = document.querySelector('#disList');
 var todoList = [];
-let count = 0;
+
 listBtn.addEventListener('click', function(e){
+    //this functions create list element and render it to the page   
+    // create li and enter the user input value as list content
+    var node = document.createElement('li');
+    var textnode = document.createTextNode(listInput.value);
+    node.appendChild(textnode);
+    //add list to the todoList array
+    todoList.push(node);
+    //render the list element to the page
+    items.appendChild(node);
+    listInput.value = '';
     
+      
+})
+//this function mark the list by changing text color and strike through
+items.addEventListener('click', (e) => e.target.classList.add('text-primary', 'text-decoration-line-through'));
+
+//this function removes an element from the list
+items.addEventListener('dblclick', function(e){
     
-    todoList.push(listInput.value);
-    console.log(todoList);
-
-    document.querySelector('#disList').innerHTML += `<li id="pList">${todoList[count]}</li>`;
-
-     count++;
+   let x = e.target.textContent;
+   // remove the element from todoList
+   todoList.forEach(element => {
+      if(element.textContent === x){
+         let index = todoList.indexOf(element);
+          todoList.splice(index, 1);
+          console.log(todoList);
+      }
+      //remove the list element from the page
+      e.target.remove();
+  });   
 })
 
-//listInput.addEventListener('mouseover', function(e){
-//    document.querySelector('#inList').style.color = 'black';
-//})
-listP.addEventListener('mouseenter', function(e){
-    console.log('I am in th window');
-})
